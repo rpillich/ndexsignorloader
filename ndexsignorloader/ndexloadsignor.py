63146e825eeeaea1e238f8f7834f844195660b40
Chris Churas <churas.camera@gmail.com>
2019-05-22 16:35:26 -0700

    sort of working version

--- ndexsignorloader/ndexloadsignor.py
+++ ndexsignorloader/ndexloadsignor.py
@@ -6,9 +6,20 @@ import sys
 import requests
 import logging
 import csv
+import json
+from datetime import datetime
+
 import pandas as pd
 from logging import config
+import networkx
+
+import ndex2
+from ndex2.client import Ndex2
+import ndexutil.tsv.tsv2nicecx2 as t2n
 from ndexutil.config import NDExUtilConfig
+from ndexutil.tsv.loaderutils import NetworkIssueReport
+from ndexutil.tsv.loaderutils import NetworkUpdator
+
 import ndexsignorloader
 
 logger = logging.getLogger(__name__)
@@ -200,7 +211,7 @@ class SignorDownloader(object):
             f.write(resp.text)
             f.flush()
 
-    def _get_pathways_map(self):
+    def get_pathways_map(self):
         """
         Gets map from :py:const:`SignorDownloader.PATHWAY_LIST_FILE` file
         :return:
@@ -286,7 +297,7 @@ class SignorDownloader(object):
             os.makedirs(self._outdir, mode=0o755)
 
         self._download_pathways_list()
-        path_map = self._get_pathways_map()
+        path_map = self.get_pathways_map()
         for key in path_map.keys():
             self._download_pathway(key, os.path.join(self._outdir,
                                                      key + '.txt'),
@@ -303,21 +314,244 @@ class SignorDownloader(object):
                                                     '.txt'))
 
 
+class DirectEdgeAttributeUpdator(NetworkUpdator):
+    """
+    Updates value of 'DIRECT' edge attribute
+    """
+    DIRECTED_ATTRIB = 'direct'
+
+    def __init__(self):
+        """
+        Constructor
+
+        """
+        super(DirectEdgeAttributeUpdator, self).__init__()
+
+    def get_description(self):
+        """
+
+        :return:
+        """
+        return 'Updates value of directed edge attribute to true and false'
+
+    def update(self, network):
+        """
+        Iterates through all edges in network updating edge attribute
+        'DIRECT' to 'YES' if value is 't' otherwise set it to 'NO'
+
+        :param network: network to examine
+        :type network: :py:class:`~ndex2.nice_cx_network.NiceCXNetwork`
+        :return: empty list
+        :rtype: list
+        """
+        if network is None:
+            return None
+
+        issues = []
+        directed_attr_name = DirectEdgeAttributeUpdator.DIRECTED_ATTRIB
+        for edge_id, edge in network.get_edges():
+            edge_attr = network.get_edge_attribute(edge_id,
+                                                    directed_attr_name)
+            if edge_attr == (None, None):
+                continue
+            network.remove_edge_attribute(edge_id, directed_attr_name)
+            if edge_attr['v'] == 't':
+
+                network.set_edge_attribute(edge_id,
+                                           directed_attr_name, True,
+                                           type='boolean')
+            else:
+                network.set_edge_attribute(edge_id,
+                                           directed_attr_name, False,
+                                           type='boolean')
+
+        return issues
+
+
+class UpdatePrefixesForNodeRepresents(NetworkUpdator):
+    """
+    Prefixes node represents with uniprot: or signor:
+    """
+
+    def __init__(self):
+        """
+        Constructor
+
+        """
+        super(UpdatePrefixesForNodeRepresents, self).__init__()
+
+    def get_description(self):
+        """
+
+        :return:
+        """
+        return 'Updates value of DIRECT edge attribute to yes and no'
+
+    def update(self, network):
+        """
+        Iterates through nodes and updates prefix for represents of
+        node based on value of 'DATABASE' node attribute. If the
+        value if 'UNIPROT' then 'uniprot:' is prefixed if not already there
+        and if 'SIGNOR' then 'signor:' is prefixed. The 'DATABASE' is
+        removed as well
+
+        :param network: network to examine
+        :type network: :py:class:`~ndex2.nice_cx_network.NiceCXNetwork`
+        :return: list of node names for which no replacement was found
+        :rtype: list
+        """
+        if network is None:
+            return None
+
+        issues = []
+        for node_id, node in network.get_nodes():
+            database = network.get_node_attribute_value(node_id, "DATABASE")
+            represents = node.get('r')
+            if database == "UNIPROT":
+                if 'uniprot:' not in represents:
+                    represents = "uniprot:" + represents
+                    node['r'] = represents
+            elif database in ["SIGNOR"]:
+                if 'signor:' not in represents:
+                    represents = "signor:" + represents
+                    node['r'] = represents
+            # in all other cases, the identifier is already prefixed
+            network.remove_node_attribute(node_id, "DATABASE")
+
+        return issues
+
+
+class NodeCompartmentUpdator(NetworkUpdator):
+    """
+    Replace any empty node Compartment attribute values with cytoplasm
+    """
+    CYTOPLASM = 'cytoplasm'
+    COMPARTMENT = 'compartment'
+
+    def __init__(self):
+        """
+        Constructor
+
+        """
+        super(NodeCompartmentUpdator, self).__init__()
+
+    def get_description(self):
+        """
+
+        :return:
+        """
+        return 'Replace any empty node compartment attribute values with cytoplasm'
+
+    def update(self, network):
+        """
+        Iterates through nodes and updates 'compartment' attribute
+        if its empty by setting it to 'cytoplasm'
+
+        :param network: network to examine
+        :type network: :py:class:`~ndex2.nice_cx_network.NiceCXNetwork`
+        :return: list of node names for which no replacement was found
+        :rtype: list
+        """
+        if network is None:
+            return None
+
+        issues = []
+        for node_id, node in network.get_nodes():
+            node_attr = network.get_node_attribute(node_id,
+                                                   NodeCompartmentUpdator.COMPARTMENT)
+            if node_attr == (None, None) or node_attr is None:
+                issues.append('Node (' + str(node_id) +
+                              ' did not have Compartment attribute')
+                network.set_node_attribute(node_id, NodeCompartmentUpdator.COMPARTMENT,
+                                           NodeCompartmentUpdator.CYTOPLASM)
+                continue
+            if node_attr['v'] is None or node_attr['v'] == '':
+                node_attr['v'] = NodeCompartmentUpdator.CYTOPLASM
+        return issues
+
+
+class SpringLayoutUpdator(NetworkUpdator):
+    """
+    Applies Spring layout
+    """
+    CARTESIAN_LAYOUT = 'cartesianLayout'
+
+    def __init__(self, scale=500.0):
+        """
+        Constructor
+
+        :param scale: scale for networkx spring layout
+        :type scale: float
+        """
+        super(SpringLayoutUpdator, self).__init__()
+
+        self._scale = scale
+
+    def get_description(self):
+        """
+
+        :return:
+        """
+        return 'Applies Spring layout to network'
+
+    def _get_cartesian_aspect(self, net_x):
+        """
+        Converts coordinates from :py:class:`networkx.Graph`
+        to NDEx aspect
+        :param net_x: network with coordinates
+        :type net_x: :py:class:`networkx.Graph`
+        :return: coordinates as list of dicts ie [{'node': <id>, 'x': <xpos>, 'y': <ypos>}]
+        :rtype: list
+        """
+        return [{'node': n,
+                 'x': float(net_x.pos[n][0]),
+                 'y': float(net_x.pos[n][1])} for n in net_x.pos]
+
+    def update(self, network):
+        """
+        Applies spring layout to network
+
+        :param network: network to examine
+        :type network: :py:class:`~ndex2.nice_cx_network.NiceCXNetwork`
+        :return: list of node names for which no replacement was found
+        :rtype: list
+        """
+        if network is None:
+            return None
+
+        issues = []
+        net_x = network.to_networkx(mode='default')
+        net_x.pos = networkx.drawing.spring_layout(net_x, scale=self._scale)
+
+        network.set_opaque_aspect(SpringLayoutUpdator.CARTESIAN_LAYOUT,
+                                  self._get_cartesian_aspect(net_x))
+        return issues
+
+
 class LoadSignorIntoNDEx(object):
     """
     Class to load content
     """
-    def __init__(self, args):
+    def __init__(self, args,
+                 downloader,
+                 updators=None):
         """
 
         :param args:
         """
         self._conf_file = args.conf
+        self._args = args
         self._profile = args.profile
-        self._outdir = os.path.abspath(theargs.datadir)
+        self._outdir = os.path.abspath(args.datadir)
         self._user = None
         self._pass = None
         self._server = None
+        self._ndex = None
+        self._loadplan = None
+        self._template = None
+        self._net_summaries = None
+        self._downloader = downloader
+        self._updators = updators
 
     def _parse_config(self):
             """
@@ -330,12 +564,57 @@ class LoadSignorIntoNDEx(object):
             self._pass = con.get(self._profile, NDExUtilConfig.PASSWORD)
             self._server = con.get(self._profile, NDExUtilConfig.SERVER)
 
+    def _get_user_agent(self):
+        """
+        Builds user agent string
+        :return: user agent string in form of ncipid/<version of this tool>
+        :rtype: string
+        """
+        return 'ndexsignorloader/' + self._args.version
+
+    def _create_ndex_connection(self):
+        """
+        creates connection to ndex
+        :return:
+        """
+        if self._ndex is None:
+            self._ndex = Ndex2(host=self._server, username=self._user,
+                               password=self._pass, user_agent=self._get_user_agent())
+
+    def _parse_load_plan(self):
+        """
+
+        :return:
+        """
+        with open(self._args.loadplan, 'r') as f:
+            self._loadplan = json.load(f)
+
+    def _load_style_template(self):
+        """
+        Loads the CX network specified by self._args.style into self._template
+        :return:
+        """
+        self._template = ndex2.create_nice_cx_from_file(os.path.abspath(self._args.style))
+
+    def _load_network_summaries_for_user(self):
+        """
+        Gets a dictionary of all networks for user account
+        <network name upper cased> => <NDEx UUID>
+        :return: dict
+        """
+        net_summaries = self._ndex.get_network_summaries_for_user(self._user)
+        self._net_summaries = {}
+        for nk in net_summaries:
+            if nk.get('name') is not None:
+                self._net_summaries[nk.get('name').upper()] = nk.get('externalId')
+
     def _get_signor_pathway_relations_df(self, pathway_id):
 
         pathway_file_path = os.path.join(self._outdir, pathway_id + '.txt')
         if not os.path.isfile(pathway_file_path):
             raise NDExLoadSignorError(pathway_file_path + ' file missing.')
-
+        if os.path.getsize(pathway_file_path) < 10:
+            raise NDExLoadSignorError(pathway_file_path + ' looks to be empty')
         with open(pathway_file_path, 'r', encoding='utf-8') as pfp:
             signor_pathway_relations_df = pd.read_csv(pfp, dtype=str, na_filter=False, delimiter='\t',
                                                       engine='python')
@@ -353,6 +632,97 @@ class LoadSignorIntoNDEx(object):
 
             return signor_pathway_relations_df
 
+    def _process_pathway(self, pathway_id, pathway_name):
+        """
+
+        :param pathway_id:
+        :return:
+        """
+        df = self._get_signor_pathway_relations_df(pathway_id)
+        # upcase column names
+        rename = {}
+        for column_name in df.columns:
+            rename[column_name] = column_name.upper()
+        df = df.rename(columns=rename)
+
+        network = t2n.convert_pandas_to_nice_cx_with_load_plan(df,
+                                                               self._loadplan)
+        report = NetworkIssueReport(pathway_name)
+
+        if self._updators is not None:
+            for updator in self._updators:
+                issues = updator.update(network)
+                report.addissues(updator.get_description(), issues)
+
+        self._add_pathway_info(network, pathway_id)
+
+        network_update_key = self._net_summaries.get(network.get_name().upper())
+
+        if network_update_key is not None:
+            network.update_to(network_update_key, self._server, self._user, self._pass,
+                              user_agent=self._get_user_agent())
+        else:
+            network.upload_to(self._server, self._user,
+                              self._pass,
+                              user_agent=self._get_user_agent())
+        return report
+
+    def _add_pathway_info(self, network, pathway_id):
+        """
+        Adds network
+        :param network:
+        :param network_id:
+        :param cytoscape_visual_properties_template_id: UUID of NDEx network to
+               extract various network attributes such as description, rightsHolder,
+               reference, rights
+
+        :return:
+        """
+        dataframe = self._get_signor_pathway_description_df(pathway_id)
+        if dataframe is None:
+            logger.warning('Skipping ' + pathway_id)
+            return
+        if not pd.isnull(dataframe.iat[0, 1]):
+            network.set_name(dataframe.iat[0, 1])
+        if not pd.isnull(dataframe.iat[0, 0]):
+            network.set_network_attribute("labels", [dataframe.iat[0, 0]], type='list_of_string')
+        if not pd.isnull(dataframe.iat[0, 3]):
+            network.set_network_attribute("author", dataframe.iat[0, 3])
+        if not pd.isnull(dataframe.iat[0, 2]):
+            network.set_network_attribute("description",
+                                          '%s %s' % (dataframe.iat[0, 2],
+                                                     self._template.get_network_attribute('description')['v']))
+
+        network.set_network_attribute('rightsHolder', 'Prof. Gianni Cesareni ')
+        network.set_network_attribute('rights', 'Attribution-ShareAlike 4.0 International (CC BY-SA 4.0')
+
+        network.set_network_attribute("reference", '<div>Perfetto L., <i>et al.</i></div><div><b>SIGNOR: a database of causal relationships between biological entities</b><i>.</i></div><div>Nucleic Acids Res. 2016 Jan 4;44(D1):D548-54</div><div><span><a href=\"\\&#34;https://doi.org/10.1093/nar/gkv1048\\&#34;\" target=\"\\&#34;\\&#34;\">doi: 10.1093/nar/gkv1048</a></span></div>')
+
+        network.set_network_attribute('dataSource',
+                                      'https://signor.uniroma2.it/pathway_browser.php?organism=&pathway_list=' +
+                                      str(pathway_id))
+
+        network.set_network_attribute("version", f"{datetime.now():%d-%b-%Y}")
+
+        disease_pathways = ['ALZHEIMER DISEASE', 'FSGS', 'NOONAN SYNDROME', 'PARKINSON DISEASE']
+
+        cancer_pathways = ['ACUTE MYELOID LEUKEMIA', 'COLORECTAL CARCINOMA', 'GLIOBLASTOMA MULTIFORME',
+                           'LUMINAL BREAST CANCER', 'MALIGNANT MELANOMA', 'PROSTATE CANCER',
+                           'RHABDOMYOSARCOMA', 'THYROID CANCER']
+
+        network.set_network_attribute("organism", "Human, 9606, Homo sapiens")
+
+        if network.get_name().upper() in disease_pathways:
+            network.set_network_attribute("networkType", "Disease Pathway")
+        elif network.get_name().upper() in cancer_pathways:
+            network.set_network_attribute("networkType", "Cancer Pathway")
+        else:
+            network.set_network_attribute("networkType", "Signalling Pathway")
+        # TODO: set “networkType” property depending on network
+        #    a. Signalling Pathway
+        #    b. Disease Pathway
+        #    c. Cancer Pathway
+
     def run(self):
         """
         Runs content loading for NDEx Signor Content Loader
@@ -360,7 +730,26 @@ class LoadSignorIntoNDEx(object):
         :return:
         """
         self._parse_config()
+        logger.debug('Parsed config: ' + self._user)
+        self._parse_load_plan()
+        self._create_ndex_connection()
+        self._load_network_summaries_for_user()
+        self._load_style_template()
+
+        pathway_map = self._downloader.get_pathways_map()
+        if pathway_map is None:
+            logger.error('Pathway map came back as None')
+            return 1
+        for key in pathway_map.keys():
+            logger.info('Processing ' + key + ' => ' + pathway_map[key])
+            try:
+                self._process_pathway(key, pathway_map[key])
+            except NDExLoadSignorError as ne:
+                logger.exception('Unable to load pathway: ' + key +
+                                 ' => ' + pathway_map[key])
+
         return 0
+
 
 def main(args):
     """
@@ -398,11 +787,17 @@ def main(args):
     try:
         _setup_logging(theargs)
         datadir = os.path.abspath(theargs.datadir)
+        downloader = SignorDownloader(theargs.signorurl,
+                                      datadir)
         if theargs.skipdownload is False:
-            downloader = SignorDownloader(theargs.signorurl,
-                                          datadir)
             downloader.download_data()
-        loader = LoadSignorIntoNDEx(theargs)
+
+        updators = [DirectEdgeAttributeUpdator(),
+                    UpdatePrefixesForNodeRepresents(),
+                    NodeCompartmentUpdator(),
+                    SpringLayoutUpdator()]
+        loader = LoadSignorIntoNDEx(theargs, downloader,
+                                    updators=updators)
         return loader.run()
     except Exception as e:
         logger.exception('Caught exception')

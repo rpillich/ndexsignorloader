ff449027df636f3d628ff32c652bf00196149bda
Chris Churas <churas.camera@gmail.com>
2019-06-07 09:52:32 -0700

    added more tests

--- tests/test_signordownloader.py
+++ tests/test_signordownloader.py
@@ -22,6 +22,28 @@ class TestSignorDownloader(unittest.TestCase):
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
+    def get_fake_signor_pf_text(self):
+        return """"SIGNOR ID";"PROT. FAMILY NAME";"LIST OF ENTITIES"
+SIGNOR-PF1;ERK1/2;"P27361,  P28482"
+SIGNOR-PF2;LPAR;"Q9HBW0,  Q9UBY5,  Q92633"
+SIGNOR-PF3;Ggamma;"P63211,  P61952,  O14610,  P50151,  P63218,  Q9UBI6,  Q9UK08,  P59768,  P50150,  Q9P2W3,  O60262,  P63215"
+SIGNOR-PF4;Gbeta;"P27361,  P28482"
+"""
+
+    def write_fake_signor_pf_file(self, destfile):
+        with open(destfile, 'w') as f:
+            f.write(self.get_fake_signor_pf_text())
+
+    def get_fake_signor_complexes_text(self):
+        return """"SIGNOR ID";"COMPLEX NAME";"LIST OF ENTITIES"
+SIGNOR-C1;NFY;"P23511,  P25208,  Q13952"
+SIGNOR-C2;mTORC2;"P68104,  Q8TB45"
+"""
+
+    def write_fake_signor_complexes_file(self, destfile):
+        with open(destfile, 'w') as f:
+            f.write(self.get_fake_signor_complexes_text())
+
     def test_get_pathway_list_file(self):
         dloader = SignorDownloader(None, 'f')
         self.assertEqual(os.path.join('f',
@@ -33,3 +55,34 @@ class TestSignorDownloader(unittest.TestCase):
         self.assertEqual(os.path.join('f',
                                       SignorDownloader.COMPLEXES_FILE),
                                       dloader.get_complexes_file())
+
+    def test_get_proteinfamily_map(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            dloader = SignorDownloader(None, temp_dir)
+            pfile = dloader.get_proteinfamily_file()
+            self.write_fake_signor_pf_file(pfile)
+            res = dloader.get_proteinfamily_map()
+            self.assertEqual(['P27361', 'P28482'], res['ERK1/2'])
+            self.assertEqual(['P27361', 'P28482'], res['SIGNOR-PF1'])
+            self.assertEqual(['Q9HBW0', 'Q9UBY5', 'Q92633'], res['LPAR'])
+            self.assertEqual(['Q9HBW0', 'Q9UBY5', 'Q92633'], res['SIGNOR-PF2'])
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_get_complexes_map(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            dloader = SignorDownloader(None, temp_dir)
+            pfile = dloader.get_complexes_file()
+            self.write_fake_signor_complexes_file(pfile)
+            res = dloader.get_complexes_map()
+            self.assertEqual(['P23511', 'P25208', 'Q13952'], res['NFY'])
+            self.assertEqual(['P23511', 'P25208', 'Q13952'], res['SIGNOR-C1'])
+
+            self.assertEqual(['P68104', 'Q8TB45'], res['mTORC2'])
+            self.assertEqual(['P68104', 'Q8TB45'], res['SIGNOR-C2'])
+
+        finally:
+            shutil.rmtree(temp_dir)
+
